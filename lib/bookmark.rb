1e29bd08cc479b63ae7bd4e5ea97f49ecaf0bb8f
davidlawes <davidlawes@hotmail.co.uk>
2018-08-28 15:20:08 +0100

    Added bookmarks erb

---
+++ lib/bookmark.rb
@@ -0,0 +1,7 @@
+class Bookmark
+
+  def self.all
+    ["www.google.com", "www.askjeeves.com", "www.yahoo.com"]
+  end
+
+end
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,8 +1,15 @@
 Capybara.app = BookmarkManager
 
-feature 'Testing route directory' do 
-  scenario 'returns hello world' do 
+feature 'Testing route directory' do
+  scenario 'returns hello world' do
     visit '/'
     expect(page).to have_content('hello world')
+  end
+end
+
+feature 'Testing Bookmarks route' do
+  scenario 'returns an array of bookmarks' do
+    visit '/bookmarks'
+    expect(page).to have_content('www.google.com')
   end
 end
